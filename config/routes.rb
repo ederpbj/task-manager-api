37d9541e1b404592272c9b3a5b28e466e2270230
Eder Pires <ederpbj@yahoo.com.br>
2018-09-22 11:15:48 +0000

    Comentando routes, arquivo de ERROs criado

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+ # namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
   	#TASKS
   	
   	#users
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   	#www.nomedosite.com/api/tasks
   	#www.nomedosite.com/api/users
 
+  	#Acesso pelo site
 	#www.site.com/api/tasks
   	#api.site.com/tasks
+
+  	#Acesso local
+  	#localhost:3000
 end
