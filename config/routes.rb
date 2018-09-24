f3867ef4bbfa3306a38f68f9229da0bf763a47dd
Eder Pires <ederpbj@yahoo.com.br>
2018-09-24 18:57:20 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+	namespace :v1 do:
+	end
+  end
+  	#TASKS
+  	
+  	#users
+
+  	#Ex:
+  	#www.nomedosite.com/api/tasks
+  	#www.nomedosite.com/api/users
+
+  	#Acesso pelo site
+	#www.site.com/api/tasks
+  	#api.site.com/tasks
+
+  	#Acesso local
+  	#localhost:3000
 end
