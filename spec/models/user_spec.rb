23625f413bdb22c1d37adf811ff142469a5583b9
Eder Pires <ederpbj@yahoo.com.br>
2018-09-24 20:40:32 +0000

    Create user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  before {@user = FactoryGirl.build(:user)}
+
+  it{expect(@user).to respond_to(:email)}
+  it{expect(@user).to respond_to(:name)}
+  it{expect(@user).to respond_to(:password)}
+  it{expect(@user).to respond_to(:password_confirmation)}
 end
