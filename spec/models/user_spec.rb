762cb80490f1cda618212fbb44d35aa1b2ba7729
Eder Pires <ederpbj@yahoo.com.br>
2018-09-25 01:19:29 +0000

    Update user_spect

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
 
-  before {@user = FactoryGirl.build(:user)}
 
-  it{expect(@user).to respond_to(:email)}
-  it{expect(@user).to respond_to(:name)}
-  it{expect(@user).to respond_to(:password)}
-  it{expect(@user).to respond_to(:password_confirmation)}
-  it{expect(@user).to be_valid}
+
+  # before {@user = FactoryGirl.build(:user)}
+
+  # it{expect(@user).to respond_to(:email)}
+  # it{expect(@user).to respond_to(:name)}
+  # it{expect(@user).to respond_to(:password)}
+  # it{expect(@user).to respond_to(:password_confirmation)}
+  # it{expect(@user).to be_valid}
+
+  #Usando subject
+  subject {build(:user)}
+  #subject = User.new
+  it{expect(subject).to respond_to(:email)}
+  it{expect(subject).to be_valid}
+
+  #outro modo
+  it{is_expected.to respond_to(:email)}  
 end
